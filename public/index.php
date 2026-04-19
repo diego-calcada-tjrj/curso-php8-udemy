292fa3fe09b5bf27079a1961e6eb5e89e03cd171
diego.calcada <diego.calcada@tjrj.jus.br>
2026-04-19 16:48:50 -0300

    Aula 2.15 - Constantes

--- public/index.php
+++ public/index.php
@@ -10,10 +10,34 @@
     </script>
 </head>
 <body>
-    
+
 <?php
-    $a = "OK";
-    echo "php funcionando " . $a;
+    define("CURSO", "PHP 8 na Udemy");
+    echo "Curso: " . CURSO;
+    echo "<br />";
+
+    class Exemplo
+    {
+        const EXEMPLO_DE_CONSTANTE = "Exemplo de constante de classe";
+        // define("CONSTANTE_DENTRO", "Constante declarada dentro");
+
+        public static function getConstValue()
+        {
+            return self::EXEMPLO_DE_CONSTANTE;
+        }
+
+        /* public static function getDefineValue()
+        {
+            return self::CONSTANTE_DENTRO;
+        } */
+    }
+
+    echo "Isso funciona: " . CURSO;
+    echo "<br />";
+    // echo "Isso NÃO funciona: " . Exemplo::CONSTANTE_DENTRO;
+    echo "Isso funciona: " . Exemplo::getConstValue();
+    // echo "Isso NÃO funciona: " . Exemplo::CONSTANTE_DENTRO;
 ?>
+
 </body>
 </html>
