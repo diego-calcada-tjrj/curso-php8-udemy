818b770b498803477def7fb4a33b3a5cae86d33e
diego.calcada <diego.calcada@tjrj.jus.br>
2026-04-19 23:50:39 -0300

    Aula 3.31 - Operadores de Arrays

--- public/index.php
+++ public/index.php
@@ -11,6 +11,28 @@
 </head>
 <body>
 
+
+<?php
+// Aula 3.31 - Operadores de Arrays
+
+// Operador de União +
+$a = ["Gol", "Latam", "Azul"];
+$b = ["Delta", "Qatar"];
+
+var_dump($a + $b);
+echo "<br/>";
+var_dump($b + $a);
+echo "<br/>";
+
+
+$c = [1,2,3];
+$d = [2 => 3, 1 => 2,0 => 1];
+
+var_dump($c == $d);
+var_dump($c === $d);
+?>
+
+<!-- 
 <?php
 // Aula 3.28 - Operadores de Execução
 
@@ -20,6 +42,6 @@ echo "<pre>$output</pre>";
 
 
 ?>
-
+ -->
 </body>
 </html>
