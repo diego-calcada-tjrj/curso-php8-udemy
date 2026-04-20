c8640a0051bc9c3570965701778a6a83bbee1c28
diego.calcada <diego.calcada@tjrj.jus.br>
2026-04-19 21:32:35 -0300

    Aula 2.16 - Tipos de Dados

--- public/index.php
+++ public/index.php
@@ -11,7 +11,75 @@
 </head>
 <body>
 
+
 <?php
+// Aula 2.16 - Tipos de Dados
+
+/* 
+// Tipos Escalares
+bool
+int
+float
+string 
+
+// Tipos Compostos
+array
+object
+callable
+iterable
+
+// Tipos Especiais
+resource
+NULL
+
+// Tipos Pseudos
+mixed
+void
+*/
+
+$ehOuNao = false;
+echo gettype($ehOuNao) . " ";
+var_dump(get_debug_type($ehOuNao));
+echo "<br/>";
+
+
+$inteiro = 3;
+echo gettype($inteiro) . " ";
+var_dump(get_debug_type($inteiro));
+echo "<br/>";
+
+$flutuante = 2.5;
+echo gettype($flutuante) . " ";
+var_dump(get_debug_type($flutuante));
+echo "<br/>";
+
+$corrente = "exemplo";
+echo gettype($corrente) . " ";
+var_dump(get_debug_type($corrente));
+echo "<br/>";
+
+
+$conversao = "3";
+echo gettype($conversao) . " ";
+$conversao = (int) $conversao;
+var_dump(get_debug_type($conversao));
+echo "<br/>";
+
+
+$definicao = "5.5";
+echo gettype($definicao) . " ";
+settype($definicao, "float");
+var_dump(get_debug_type($definicao));
+echo "<br/>";
+
+
+
+?>
+
+
+<!-- <?php
+// Aula 2.15 - Constantes
+
     define("CURSO", "PHP 8 na Udemy");
     echo "Curso: " . CURSO;
     echo "<br />";
@@ -37,7 +105,7 @@
     // echo "Isso NÃO funciona: " . Exemplo::CONSTANTE_DENTRO;
     echo "Isso funciona: " . Exemplo::getConstValue();
     // echo "Isso NÃO funciona: " . Exemplo::CONSTANTE_DENTRO;
-?>
+?> -->
 
 </body>
 </html>
